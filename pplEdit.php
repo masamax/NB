554924eb426cffb1276b006da6ca7f63c89dd996
masamax <admin@cilqy.com>
2016-05-30 13:56:47 -0700

    Create pplEdit.php

---
+++ pplEdit.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once("nbConf.php");
+
+$apiURL = '/api/v1/people/push';
+$url = $nbURL.$apiURL.'?access_token='.$nbToken;
+
+$data = array(
+  'person' => array(
+  'email' => $_REQUEST['email'],
+  'email_opt_in' => $_REQUEST['optin'],
+  ),
+);
+$json_data = json_encode($data);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT'); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+curl_setopt($ch, CURLOPT_TIMEOUT, '10'); 
+curl_setopt($ch, CURLOPT_HTTPHEADER, 
+array("Content-Type: application/json","Accept: application/json"));
+curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
+$json_response = curl_exec($ch);
+curl_close($ch);
+
+$response = json_decode($json_response, true);
+print_r($response);
+
+?>
